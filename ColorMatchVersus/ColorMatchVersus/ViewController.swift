656762f17c5a06a194ee3a85b1170e05dd3e0af5
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-09-05 00:59:17 -0300

    Remove unnecessary code and comments

--- ColorMatchVersus/ColorMatchVersus/ViewController.swift
+++ ColorMatchVersus/ColorMatchVersus/ViewController.swift
@@ -1,19 +1,4 @@
-//
-//  ViewController.swift
-//  ColorMatchVersus
-//
-//  Created by Lucas Kuipers on 05/09/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
